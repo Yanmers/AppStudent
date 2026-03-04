af3a9eb89b2e166fc8fa4e1342fb157aefc55728
Yanmers <mercedesyaniel@gmail.com>
2026-03-04 11:15:13 -0400

    Update

---
+++ Data/Config/RolePrivilegeConf.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppStudent.Data.Config
+{
+    public class RolePrivilegeConf : IEntityTypeConfiguration<RolePrivilege>
+    {
+        public void Configure(EntityTypeBuilder<RolePrivilege> builder)
+        {
+
+            builder.ToTable("RolePrivileges");
+            builder.HasKey(n => n.Id);
+
+            builder.Property(n => n.Id).UseIdentityColumn();
+
+            builder.Property(n => n.RolePrivilegeName).IsRequired();
+            builder.Property(n => n.Description);
+            builder.Property(n => n.RoleId).IsRequired();
+            builder.Property(n => n.IsActive).IsRequired();
+            builder.Property(n => n.IsDelete).IsRequired();
+            builder.Property(n => n.CreateDate).IsRequired();
+
+        }
+    }
+}
+
---
+++ Data/RolePrivilege.cs
@@ -0,0 +1,17 @@
+﻿namespace AppStudent.Data
+{
+    public class RolePrivilege
+    {
+        public int Id { get; set; }
+        public string RolePrivilegeName { get; set; }
+        public string Description { get; set; }
+
+        public int RoleId { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsDelete { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
