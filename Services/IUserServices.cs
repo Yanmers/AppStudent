56650d5c03aa6b06a6afb4b86027fb433ce4c687
Yanmers <mercedesyaniel@gmail.com>
2026-03-16 22:08:35 -0400

    Add UserServices

---
+++ Services/IUserServices.cs
@@ -0,0 +1,16 @@
+﻿using Octokit;
+
+namespace AppStudent.Services
+{
+    public interface IUserServices
+    {
+        Task<IEnumerable<User>> GetAllUserAsync();
+
+        Task<User> GetUserByIdAsync(int id);
+
+        Task<User> GetUserByName(string UserName);
+
+        Task<User> UpdateUserAsync(User user, int id);
+        Task<bool> DeleteUserAsync(int id);
+    }
+}
---
+++ Services/UserServices.cs
@@ -0,0 +1,33 @@
+﻿using Octokit;
+
+namespace AppStudent.Services
+{
+    public class UserServices : IUserServices
+    {
+
+        public Task<bool> DeleteUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByName(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateUserAsync(User user, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
